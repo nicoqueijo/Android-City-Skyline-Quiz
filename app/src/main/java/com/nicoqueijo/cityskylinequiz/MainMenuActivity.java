45620a30f3be2d6a2965d9a0e275c3ea907972ac
Nicolas <nicoqueijo@hotmail.com>
2017-07-17 17:08:05 -0400

    added participating country flags

--- app/src/main/java/com/nicoqueijo/cityskylinequiz/MainMenuActivity.java
+++ app/src/main/java/com/nicoqueijo/cityskylinequiz/MainMenuActivity.java
@@ -9,5 +9,9 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_main);
+
+
+        // caching example
+        //Picasso.with(MainActivity.this).load(url).fetch();
     }
 }
