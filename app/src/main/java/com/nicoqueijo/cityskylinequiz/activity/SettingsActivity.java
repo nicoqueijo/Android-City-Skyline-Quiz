bb487c03fa5e06993d05914b2b00e4ada18900cb
Nicolas Queijo <nicoqueijo@hotmail.com>
2017-08-01 16:58:51 -0400

    added click listeners for each item in the settings

--- app/src/main/java/com/nicoqueijo/cityskylinequiz/activity/SettingsActivity.java
+++ app/src/main/java/com/nicoqueijo/cityskylinequiz/activity/SettingsActivity.java
@@ -2,15 +2,58 @@ package com.nicoqueijo.cityskylinequiz.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import com.nicoqueijo.cityskylinequiz.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private LinearLayout mThemeView;
+    private LinearLayout mLanguageView;
+    private LinearLayout mScoresView;
+    private Switch mThemeSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mThemeView = (LinearLayout) findViewById(R.id.container_settings_theme);
+        mLanguageView = (LinearLayout) findViewById(R.id.container_settings_language);
+        mScoresView = (LinearLayout) findViewById(R.id.container_settings_scores);
+        mThemeSwitch = (Switch) findViewById(R.id.switch_theme);
+
+        mThemeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mThemeSwitch.setChecked(!mThemeSwitch.isChecked());
+            }
+        });
+
+        mLanguageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mScoresView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mThemeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do the same as themeview listener
+            }
+        });
+
+
     }
 }
