9df7be79dbd7167b427d9d6fa5e223986de5efee
Nicolas Queijo <nicoqueijo@hotmail.com>
2017-10-02 18:04:37 -0400

    Implemented the skeleton of the SQLite database that will track the highscores

---
+++ app/src/main/java/com/nicoqueijo/cityskylinequiz/databases/HighScoresHelper.java
@@ -0,0 +1,78 @@
+package com.nicoqueijo.cityskylinequiz.databases;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class HighScoresHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "highscoresdatabase.db";
+    private static final String TABLE_NAME = "HIGHSCORES";
+    private static final int DATABASE_VERSION = 1;
+    private static final String MODE = "Mode";
+    private static final String SCORE = "Score";
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + MODE + " VARCHAR(25) PRIMARY KEY, "
+            + SCORE + " DECIMAL(5,2));";
+    private Context context;
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use to open or create the database
+     * @param name    of the database file, or null for an in-memory database
+     * @param factory to use for creating cursor objects, or null for the default
+     * @param version number of the database (starting at 1); if the database is older,
+     *                {@link #onUpgrade} will be used to upgrade the database; if the database is
+     *                newer, {@link #onDowngrade} will be used to downgrade the database
+     */
+    public HighScoresHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                            int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(CREATE_TABLE);
+        } catch (SQLException e) {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     * <p>
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
