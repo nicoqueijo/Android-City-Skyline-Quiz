8023548aba9e8a63e4ea9e233a7a34a513c058d4
Nicolas Queijo <nicoqueijo@hotmail.com>
2017-10-12 17:45:04 -0400

    moved the alert dialogs to their own method

--- app/src/main/java/com/nicoqueijo/cityskylinequiz/activities/MainMenuActivity.java
+++ app/src/main/java/com/nicoqueijo/cityskylinequiz/activities/MainMenuActivity.java
@@ -75,40 +75,7 @@ public class MainMenuActivity extends AppCompatActivity {
         mActionBar.setIcon(R.mipmap.ic_launcher);
         mActionBar.setTitle(R.string.actionbar_app_name);
 
-        // If first time launching app, show an alert dialog advising
-        // user to allow a minute for image resources to load.
-        if (mSharedPreferences.getBoolean("first_launch", true)) {
-
-            final AlertDialog.Builder translationDialog = new AlertDialog.Builder(this);
-            final AlertDialog.Builder welcomeDialog = new AlertDialog.Builder(this);
-            welcomeDialog.setTitle(getResources().getString(R.string.welcome));
-            welcomeDialog.setMessage(getResources().getString(R.string.load_resources));
-            welcomeDialog.setIcon(R.drawable.ic_welcome);
-            welcomeDialog.setCancelable(false);
-            welcomeDialog.setPositiveButton(getResources().getString(R.string.ok),
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                            translationDialog.setTitle(getResources().getString(R.string.language_disclaimer_title));
-                            translationDialog.setMessage(getResources().getString(R.string.language_disclaimer_message));
-                            translationDialog.setIcon(R.drawable.ic_translate);
-                            translationDialog.setCancelable(false);
-                            translationDialog.setPositiveButton(getResources().getString(R.string.ok),
-                                    new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            dialog.dismiss();
-                                        }
-                                    });
-                            translationDialog.show();
-                        }
-                    });
-            welcomeDialog.show();
-
-            editor.putBoolean("first_launch", false);
-            editor.commit();
-        }
+        showFirstLaunchDialogs(editor);
 
         cities = new ArrayList<>();
         parseJsonAndCreateCityObjects();
@@ -338,6 +305,51 @@ public class MainMenuActivity extends AppCompatActivity {
                     .getStringResourceByName(city.getCityName(), this));
             city.setCountryNameInCurrentLanguage(ResourceByNameRetriever
                     .getStringResourceByName(city.getCountryName(), this));
+        }
+    }
+
+    /**
+     * Shows a couple Alert Dialogs to the user if it's their first time launching the app. The
+     * dialogs are a welcome message asking the user to allow a minute for the image resources to
+     * load and a disclaimer apologizing to the user for possible mistranslated non-english strings.
+     * The dialogs appear consecutively. Once the welcome dialog is dismissed, the disclaimer
+     * dialog is generated after.
+     *
+     * @param editor the SharedPreferences Editor to save and track if it's the first time the
+     *               user launches the app.
+     */
+    private void showFirstLaunchDialogs(SharedPreferences.Editor editor) {
+        if (mSharedPreferences.getBoolean("first_launch", true)) {
+            final AlertDialog.Builder translationDialog = new AlertDialog.Builder(this);
+            final AlertDialog.Builder welcomeDialog = new AlertDialog.Builder(this);
+            welcomeDialog.setTitle(getResources().getString(R.string.welcome));
+            welcomeDialog.setMessage(getResources().getString(R.string.load_resources));
+            welcomeDialog.setIcon(R.drawable.ic_welcome);
+            welcomeDialog.setCancelable(false);
+            welcomeDialog.setPositiveButton(getResources().getString(R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            translationDialog.setTitle(getResources().getString(R.string
+                                    .language_disclaimer_title));
+                            translationDialog.setMessage(getResources().getString(R.string
+                                    .language_disclaimer_message));
+                            translationDialog.setIcon(R.drawable.ic_translate);
+                            translationDialog.setCancelable(false);
+                            translationDialog.setPositiveButton(getResources().getString(R.string.ok),
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                            translationDialog.show();
+                        }
+                    });
+            welcomeDialog.show();
+            editor.putBoolean("first_launch", false);
+            editor.commit();
         }
     }
 }
